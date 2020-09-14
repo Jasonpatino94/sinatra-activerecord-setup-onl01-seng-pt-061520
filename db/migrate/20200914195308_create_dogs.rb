a33a64a594e1df12b30ead3629e1922e1a41c623
jasonpatino94 <jason.patino94@gmail.com>
2020-09-14 16:04:21 -0400

    Done.

--- db/migrate/20200914195308_create_dogs.rb
+++ db/migrate/20200914195308_create_dogs.rb
@@ -10,4 +10,11 @@ class CreateDogs < ActiveRecord::Migration[5.2]
   def down
     drop_table :dogs
   end
+
+  def def change 
+    create_table :dogs do |t|
+      t.string :name 
+      t.string :breed
+    end
+  end
 end
